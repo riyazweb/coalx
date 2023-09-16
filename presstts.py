ff10f47eb0c475fdd49c718271bf160d67cf43c1
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-09-16 19:53:15 +0530

    Update presstts.py

--- presstts.py
+++ presstts.py
@@ -96,6 +96,15 @@ def main():
 
             
             cop = f"{OP}"
+     #for clean pure voice
+  # from elevenlabs import generate, play, set_api_key, save
+
+     #       voice = generate(
+      #          text=f"{cop}",
+       #         voice="Bella",
+        #        model="eleven_multilingual_v2"
+         #   )
+          #  save(voice,'data.mp3')
 
             speak(cop)
 
